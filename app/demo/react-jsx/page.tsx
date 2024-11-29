ed89ed6f4dbc7707210c7dc4c19d82ef3f78e1a8
takeseem <takeseem@gmail.com>
2024-11-29 15:29:06 +0800

    描述 UI：将 Props 传递给组件

--- app/demo/react-jsx/page.tsx
+++ app/demo/react-jsx/page.tsx
@@ -15,12 +15,48 @@ export default function Page() {
         <div>
           <TodoList />
         </div>
+        <div>
+          <Profile2 />
+        </div>
       </div>
       <hr style={{ margin: "1rem 0 0", }}/>
       <main>
         <ProjectPageInfo project={reactJsx} />
       </main>
     </>
+  );
+}
+
+function Profile2() {
+  return (
+    <Card>
+      <Avatar person={{ name: 'Katsuko Saruhashi', imageId: 'YfeOqp2' }} size="100" />
+    </Card>
+  );
+}
+
+function Card({ children }: { children: React.ReactNode }) {
+  return (
+    <div style={{
+      background: "#fff", width: "fit-content",
+      padding: "1rem", borderRadius: "1rem", margin: "1rem",
+    }}>
+      {children}
+    </div>
+  );
+}
+
+type Person = {
+  name: string;
+  imageId: string;
+}
+
+const getImageUrl = (person: Person, size = 's') => `https://i.imgur.com/${person.imageId}${size}.jpg`;
+function Avatar({ person, size }: { person: Person; size: string }) {
+  return (
+    <img style={{ borderRadius: "50%" }}
+      src={getImageUrl(person)} alt={person.name}
+      width={size} height={size} />
   );
 }
 
