5188c61d33f7b882231e708e9622bdab1670ac95
takeseem <takeseem@gmail.com>
2024-11-29 15:05:31 +0800

    描述 UI：第一个组件

--- app/demo/react-jsx/page.tsx
+++ app/demo/react-jsx/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 
 import { reactJsx } from "@/app/lib/placeholder-data";
@@ -8,7 +9,8 @@ export default function Page() {
   return (
     <>
       <div>
-        <div style={{ display: "flex", gap: "1rem", padding: "0.5rem", }}>
+        <div>
+          <Gallery />
         </div>
       </div>
       <hr style={{ margin: "1rem 0 0", }}/>
@@ -16,5 +18,24 @@ export default function Page() {
         <ProjectPageInfo project={reactJsx} />
       </main>
     </>
+  );
+}
+
+
+function Gallery() {
+  return (
+    <section>
+      <h1>了不起的科学家</h1>
+      <div style={{ display: "flex", padding: "1rem 0"}}>
+        <Profile />
+        <Profile />
+        <Profile />
+      </div>
+    </section>
+  );
+}
+function Profile() {
+  return (
+    <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
   );
 }
