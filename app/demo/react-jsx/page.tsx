62dadc95dec4ada8383083267eacbd117e5da040
takeseem <takeseem@gmail.com>
2024-11-29 15:52:13 +0800

    描述 UI：渲染列表

--- app/demo/react-jsx/page.tsx
+++ app/demo/react-jsx/page.tsx
@@ -4,26 +4,30 @@
 import { reactJsx } from "@/app/lib/placeholder-data";
 import { ProjectPageInfo } from "@/app/ui/demo/ProjectPageInfo";
 
-
+const h1Style = { fontSize: "1.5rem", fontWeight: "bold", };
 export default function Page() {
   return (
     <>
       <div>
         <div>
-          <h1 style={{ fontSize: "1.5rem", fontWeight: "bold", }}>第一个组件</h1>
+          <h1 style={h1Style}>第一个组件</h1>
           <Gallery />
         </div>
         <div>
-          <h1 style={{ fontSize: "1.5rem", fontWeight: "bold", }}>TodoList</h1>
+          <h1 style={h1Style}>TodoList</h1>
           <TodoList />
         </div>
         <div>
-          <h1 style={{ fontSize: "1.5rem", fontWeight: "bold", }}>将 Props 传递给组件</h1>
+          <h1 style={h1Style}>将 Props 传递给组件</h1>
           <Profile2 />
         </div>
         <div>
-          <h1 style={{ fontSize: "1.5rem", fontWeight: "bold", }}>条件渲染</h1>
+          <h1 style={h1Style}>条件渲染</h1>
           <PackingList />
+        </div>
+        <div>
+          <h1 style={h1Style}>渲染列表</h1>
+          <PeopleList />
         </div>
       </div>
       <hr style={{ margin: "1rem 0 0", }}/>
@@ -31,6 +35,68 @@ export default function Page() {
         <ProjectPageInfo project={reactJsx} />
       </main>
     </>
+  );
+}
+
+type People = {
+  id: number;
+  name: string;
+  profession: string;
+  accomplishment: string;
+  imageId: string;
+};
+const peoples: People[] = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+  accomplishment: 'spaceflight calculations',
+  imageId: 'MK3eW3A'
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+  accomplishment: 'discovery of Arctic ozone hole',
+  imageId: 'mynHUSa'
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+  accomplishment: 'electromagnetism theory',
+  imageId: 'bE7W1ji'
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',
+  accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+  imageId: 'IOjWm71'
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+  accomplishment: 'white dwarf star mass calculations',
+  imageId: 'lrWQx8l'
+}];
+
+function PeopleList() {
+  return (
+    <article>
+      <h1>科学家</h1>
+      <ul style={{ display: "flex", flexDirection: "column", gap: "1rem", padding: "1rem", }}>
+        {peoples.map((person) => (
+          <li key={person.id} style={{
+            display: "flex", gap: "1rem",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}>
+            <Avatar person={person} size="100" />
+            <p style={{ flex: "0 1 20rem", }}>
+              <b>{person.name}</b>
+              {` ${person.profession} known for ${person.accomplishment}`}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </article>
   );
 }
 
