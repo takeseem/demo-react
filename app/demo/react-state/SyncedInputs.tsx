80f43bb3748c72c9024b99bb9cf563cafe5752df
takeseem <takeseem@gmail.com>
2024-12-04 21:03:56 +0800

    完成 挑战: 同步输入状态

--- app/demo/react-state/SyncedInputs.tsx
+++ app/demo/react-state/SyncedInputs.tsx
@@ -1,15 +1,7 @@
+import { bgStyle } from '@/app/lib/definitions';
 import { useState } from 'react';
 
 export default function SyncedInputs() {
-  return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem", }}>
-      <Input label="第一个输入框" />
-      <Input label="第二个输入框" />
-    </div>
-  );
-}
-
-function Input({ label }: { label: string }) {
   const [text, setText] = useState('');
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -17,12 +9,26 @@ function Input({ label }: { label: string }) {
   }
 
   return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem", }}>
+      <Input label="第一个输入框" text={text} onChange={handleChange} />
+      <Input label="第二个输入框" text={text} onChange={handleChange} />
+    </div>
+  );
+}
+
+function Input({ label, text, onChange, }: {
+  label: string;
+  text: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+ }) {
+
+  return (
     <label>
       {label}
       {' '}
-      <input
+      <input style={bgStyle}
         value={text}
-        onChange={handleChange}
+        onChange={onChange}
       />
     </label>
   );
