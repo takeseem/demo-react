9fcf6c2033f9f69c8f68a8f34eb83c98df39cba0
takeseem <takeseem@gmail.com>
2024-12-05 06:04:33 +0800

    fix 挑战: 修复丢失的输入框文本

--- app/demo/react-state/MissText.tsx
+++ app/demo/react-state/MissText.tsx
@@ -3,22 +3,15 @@ import { useState } from 'react';
 
 export default function MissText() {
   const [showHint, setShowHint] = useState(false);
-  if (showHint) {
-    return (
-      <div style={{display: "flex", gap: "0.5rem", flexDirection: "column", alignItems: "flex-start", padding: "0.5rem", }}>
-        <p><i>提示：你最喜欢的城市？</i></p>
-        <Form />
-        <button style={btnStyle} onClick={() => {
-          setShowHint(false);
-        }}>隐藏提示</button>
-      </div>
-    );
-  }
   return (
     <div style={{display: "flex", gap: "0.5rem", flexDirection: "column", alignItems: "flex-start", padding: "0.5rem", }}>
+      {showHint
+        ? <p><i>提示：你最喜欢的城市？</i></p>
+        : null
+      }
       <Form />
       <button style={btnStyle} onClick={() => {
-        setShowHint(true);
+        setShowHint(!showHint);
       }}>显示提示</button>
     </div>
   );
