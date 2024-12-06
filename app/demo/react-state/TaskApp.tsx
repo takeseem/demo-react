c93c87ae0491a2d0516c5fd398f83295fd040873
takeseem <takeseem@gmail.com>
2024-12-06 11:25:39 +0800

    tasks 和 dispatch 都迁移到 TaskContext.tsx 文件中，TaskApp 中用占位符代替

--- app/demo/react-state/TaskApp.tsx
+++ app/demo/react-state/TaskApp.tsx
@@ -1,76 +1,20 @@
-import { useReducer } from 'react';
 import AddTask from "./TaskAdd";
 import TaskList from './TaskList';
-import { initialTasks, Task, TaskReducerAction } from './TaskContext';
-
-let nextId = 3;
+import TasksProvider from './TaskContext';
 
 export default function TaskApp() {
-  const [tasks, dispatch] = useReducer(
-    tasksReducer,
-    initialTasks
-  );
-
-  function handleAddTask(text: string) {
-    dispatch({
-      type: 'added',
-      id: nextId++,
-      text: text,
-    });
-  }
-
-  function handleChangeTask(task: Task) {
-    dispatch({
-      type: 'changed',
-      task: task
-    });
-  }
-
-  function handleDeleteTask(taskId: number) {
-    dispatch({
-      type: 'deleted',
-      id: taskId
-    });
-  }
-
   return (
-    <>
+    <TasksProvider>
       <h1>Day off in Kyoto</h1>
       <AddTask
-        onAddTask={handleAddTask}
+        onAddTask={() => {}}
       />
       <TaskList
-        tasks={tasks}
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
+        tasks={[]}
+        onChangeTask={() => {}}
+        onDeleteTask={() => {}}
       />
-    </>
+    </TasksProvider>
   );
 }
 
-function tasksReducer(tasks: Task[], action: TaskReducerAction): Task[] {
-  switch (action.type) {
-    case 'added': {
-      return [...tasks, {
-        id: action.id as number,
-        text: action.text as string,
-        done: false
-      }];
-    }
-    case 'changed': {
-      return tasks.map(t => {
-        if (t.id === action?.task?.id) {
-          return action.task;
-        } else {
-          return t;
-        }
-      });
-    }
-    case 'deleted': {
-      return tasks.filter(t => t.id !== action.id);
-    }
-    default: {
-      throw Error('Unknown action: ' + action.type);
-    }
-  }
-}
--- app/demo/react-state/TaskContext.tsx
+++ app/demo/react-state/TaskContext.tsx
@@ -1,3 +1,4 @@
+import { createContext, Dispatch, ReactNode, useReducer } from "react";
 
 export type Task = {
   id: number;
@@ -17,3 +18,44 @@ export type TaskReducerAction = {
   text?: string;
   task?: Task;
 };
+
+const TasksDispatchContext = createContext<Dispatch<TaskReducerAction>>(() => { });
+const TasksContext = createContext<Task[]>([]);
+
+export default function TasksProvider({ children, }: { children: ReactNode, }) {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+  return (
+    <TasksContext.Provider value={tasks}>
+      <TasksDispatchContext.Provider value={dispatch}>
+        {children}
+      </TasksDispatchContext.Provider>
+    </TasksContext.Provider>
+  );
+}
+
+function tasksReducer(tasks: Task[], action: TaskReducerAction): Task[] {
+  switch (action.type) {
+    case 'added': {
+      return [...tasks, {
+        id: action.id as number,
+        text: action.text as string,
+        done: false
+      }];
+    }
+    case 'changed': {
+      return tasks.map(t => {
+        if (t.id === action?.task?.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case 'deleted': {
+      return tasks.filter(t => t.id !== action.id);
+    }
+    default: {
+      throw Error('Unknown action: ' + action.type);
+    }
+  }
+}
