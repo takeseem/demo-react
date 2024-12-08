d5a3a8c1f3ced207264b6e155548009c3afe510c
takeseem <takeseem@gmail.com>
2024-12-08 15:51:32 +0800

    定一个挑战：修复坏掉的聊天输入框

--- app/demo/react-escape/Demo1.tsx
+++ app/demo/react-escape/Demo1.tsx
@@ -1,8 +1,8 @@
-import { btnStyle } from "@/app/lib/definitions";
-import { useRef } from "react";
+import { bgStyle, btnStyle } from "@/app/lib/definitions";
+import { useRef, useState } from "react";
 
 export function Counter() {
-  let ref = useRef(0);
+  const ref = useRef(0);
 
   return (
     <button style={btnStyle} onClick={() => {
@@ -10,5 +10,75 @@ export function Counter() {
       ref.current += 1;
       alert('当前点击次数：' + ref.current);
     }}>点击我！</button>
+  );
+}
+
+export function Stopwatch() {
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [now, setNow] = useState(Date.now())
+  const time = startTime ? (now - startTime) / 1000 : 0;
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  function clear() {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  }
+  return (
+    <>
+      <h1>秒表：{time.toFixed(3)}</h1>
+      <button style={btnStyle} onClick={() => {
+        const cur = Date.now();
+        setNow(cur);
+        setStartTime(cur);
+
+        clear();
+        intervalRef.current = setInterval(() => {
+          setNow(Date.now());
+        }, 10)
+      }}>开始</button>
+      <button style={btnStyle} onClick={() => {
+        clear();
+      }}>停止</button>
+    </>
+  );
+}
+
+export default function Chat() {
+  const [text, setText] = useState('');
+  const [isSending, setIsSending] = useState(false);
+  let timeoutID = null;
+
+  function handleSend() {
+    setIsSending(true);
+    timeoutID = setTimeout(() => {
+      alert('已发送！');
+      setIsSending(false);
+    }, 3000);
+  }
+
+  function handleUndo() {
+    setIsSending(false);
+    clearTimeout(timeoutID);
+  }
+
+  return (
+    <>
+      <input style={bgStyle}
+        disabled={isSending}
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <button style={btnStyle}
+        disabled={isSending}
+        onClick={handleSend}>
+        {isSending ? '发送中……' : '发送'}
+      </button>
+      {isSending &&
+        <button onClick={handleUndo} style={btnStyle}>
+          撤销
+        </button>
+      }
+    </>
   );
 }
--- app/demo/react-escape/page.tsx
+++ app/demo/react-escape/page.tsx
@@ -4,7 +4,7 @@
 import { h1Style } from "@/app/lib/definitions";
 import { reactEscape } from "@/app/lib/placeholder-data";
 import { ProjectPageInfo } from "@/app/ui/demo/ProjectPageInfo";
-import { Counter } from "./Demo1";
+import Chat, { Counter, Stopwatch } from "./Demo1";
 
 export default function Page() {
   return (
@@ -14,10 +14,19 @@ export default function Page() {
         <hr style={{ margin: "1rem 0", }} />
         <div>
           <h1 style={h1Style}>使用 ref 引用值</h1>
-          <p>说明</p>
-          <div>
-            <Counter />
-          </div>
+          <p>
+            <a target="_blank" href="https://zh-hans.react.dev/learn/referencing-values-with-refs">
+              官方文档
+            </a>
+          </p>
+          <p>使用 useRef 存储点击次数：let ref = useRef(0);</p>
+          <Counter />
+
+          <p>{`使用 useRef 存储 intervalId：const intervalRef = useRef<NodeJS.Timeout | null>(null);`}</p>
+          <Stopwatch />
+
+          <p>挑战: 修复坏掉的聊天输入框 </p>
+          <Chat />          
         </div>
       </div>
     </main>
