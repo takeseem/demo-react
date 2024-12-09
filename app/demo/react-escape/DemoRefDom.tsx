8846529ba411b1818c906bcce19fa0e6dec870f0
takeseem <takeseem@gmail.com>
2024-12-09 09:44:33 +0800

    完成挑战1

--- app/demo/react-escape/DemoRefDom.tsx
+++ app/demo/react-escape/DemoRefDom.tsx
@@ -1,12 +1,19 @@
 import { btnStyle } from '@/app/lib/definitions';
-import { useState, } from 'react';
+import { useRef, useState, } from 'react';
 
 export function VideoPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   function handleClick() {
     const nextIsPlaying = !isPlaying;
     setIsPlaying(nextIsPlaying);
+    console.log(new Date().toISOString(), 'handleClick', nextIsPlaying);
+    if (nextIsPlaying) {
+      videoRef.current?.play();
+    } else {
+      videoRef.current?.pause();
+    }
   }
 
   return (
@@ -14,7 +21,11 @@ export function VideoPlayer() {
       <button onClick={handleClick} style={btnStyle}>
         {isPlaying ? '暂停' : '播放'}
       </button>
-      <video width="250">
+      <video width="250"
+        ref={videoRef}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      >
         <source
           src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
           type="video/mp4"
