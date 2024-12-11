83f8a6b394181737366066ca171a11e5c0ccab9c
takeseem <takeseem@gmail.com>
2024-12-12 03:30:55 +0800

    完成提取 useCounter

--- app/demo/react-escape/DemoHook.tsx
+++ app/demo/react-escape/DemoHook.tsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 
 // 第 1 个挑战 共 5 个挑战: 提取 useCounter Hook
 export function CounterHookApp1() {
-  const [count, setCount] = useState(0);
+  const count = useCounter();
+  return <h1>Seconds passed: {count}</h1>;
+}
+
+function useCounter(initCount = 0) {
+  const [count, setCount] = useState(initCount);
   useEffect(() => {
     const id = setInterval(() => {
       setCount(c => c + 1);
     }, 1000);
     return () => clearInterval(id);
-  }, []);
-  return <h1>Seconds passed: {count}</h1>;
+  }, [initCount]);
+  return count;
 }
